511b0623470ed53d34cd91c23ce11a850950c4a8
Qualquer Valor <qualquervalor@gmail.com>
2013-11-27 00:26:50 -0600

    generalized compare to takefunctions as inputs.  Also moved the twitter client into the user initialize

--- follower_clash.rb
+++ follower_clash.rb
@@ -6,3 +6,12 @@ user1 = UserComparer::User.new('ornellasmike')
 user2 = UserComparer::User.new('techpeace')
 puts "#{user1.username}'s follower count: #{user1.followers}"
 puts "#{user2.username}'s follower count: #{user2.followers}"
+puts "#{user1.username}'s freind count: #{user1.following}"
+puts "#{user2.username}'s friend count: #{user2.following}"
+puts "#{user1.username}'s tweet count: #{user1.tweets}"
+puts "#{user2.username}'s tweet count: #{user2.tweets}"
+
+comparer = UserComparer::Comparer.new(user1, user2)
+puts "And the user with the most followers is: #{comparer.compare(user1.followers,user2.followers)}"
+puts "And the user with the most friends is: #{comparer.compare(user1.following,user2.following)}"
+puts "And the user with the most tweets is: #{comparer.compare(user1.tweets,user2.tweets)}"
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -1,34 +1,41 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'twitter'
+require 'pry'
 
 module UserComparer
   class User
-    attr_reader :username
+    attr_reader :username, :client
     def initialize(username)
         @username = username
+        @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token         = ENV['OAUTH_TOKEN']
+        config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+      end
     end
-    def followers
-      client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token         = ENV['OAUTH_TOKEN']
-      config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+    def followers      
+      client.user(username).followers_count
     end
-
-    client.user(@username).followers_count
+    def following
+      client.user(@username).friends_count
+    end
+    def tweets
+      client.user(@username).tweets_count
     end
   end
+
 
   class Comparer
     attr_reader :user1, :user2
     def initialize(user1,user2)
         @user1, @user2 = user1, user2
     end
-    def compare
-        if (user1.followers < user2.followers)
+    def compare(fun1,fun2)
+        if (fun1 < fun2)
             user2.username
-        elsif (user1.followers > user2.followers)
+        elsif (fun1 > fun2)
             user1.username
         else
             "equal number"
